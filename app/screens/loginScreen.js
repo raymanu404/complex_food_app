a695a9503bc65c2fb3531615e66fc9e9106a415d
raymanu404 <manu.caprariu.brediceanu@gmail.com>
2024-03-17 00:47:18 +0200

    update react, get rid of google sign in package for now

--- app/screens/loginScreen.js
+++ app/screens/loginScreen.js
@@ -21,36 +21,36 @@ import api_axios from '../../config/api/api_axios';
 // import {LoginButton, AccessToken} from 'react-native-fbsdk-next';
 
 import {Icon} from 'react-native-elements';
-import {
-  GoogleSignin,
-  GoogleSigninButton,
-  statusCodes,
-} from '@react-native-google-signin/google-signin';
+// import {
+//   GoogleSignin,
+//   GoogleSigninButton,
+//   statusCodes,
+// } from '@react-native-google-signin/google-signin';
 
-GoogleSignin.configure({
-  // scopes: ['https://www.googleapis.com/auth/drive.readonly'], // [Android] what API you want to access on behalf of the user, default is email and profile
-  webClientId:
-    '562986888695-eqf2tin1l14kliee4r92lstq30crdki9.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
-  offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
-});
+// GoogleSignin.configure({
+//   // scopes: ['https://www.googleapis.com/auth/drive.readonly'], // [Android] what API you want to access on behalf of the user, default is email and profile
+//   webClientId:
+//     '562986888695-eqf2tin1l14kliee4r92lstq30crdki9.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
+//   offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
+// });
 
-const signIn = async () => {
-  try {
-    await GoogleSignin.hasPlayServices();
-    const userInfo = await GoogleSignin.signIn();
-    console.log(userInfo);
-  } catch (error) {
-    if (error.code === statusCodes.SIGN_IN_CANCELLED) {
-      // user cancelled the login flow
-    } else if (error.code === statusCodes.IN_PROGRESS) {
-      // operation (e.g. sign in) is in progress already
-    } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
-      // play services not available or outdated
-    } else {
-      // some other error happened
-    }
-  }
-};
+// const signIn = async () => {
+//   try {
+//     await GoogleSignin.hasPlayServices();
+//     const userInfo = await GoogleSignin.signIn();
+//     console.log(userInfo);
+//   } catch (error) {
+//     if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+//       // user cancelled the login flow
+//     } else if (error.code === statusCodes.IN_PROGRESS) {
+//       // operation (e.g. sign in) is in progress already
+//     } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+//       // play services not available or outdated
+//     } else {
+//       // some other error happened
+//     }
+//   }
+// };
 const {height} = Dimensions.get('screen');
 const width = Dimensions.get('screen').width;
 
